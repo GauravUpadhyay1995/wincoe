c993a451a967aa7997b87c27873f4722a74b1d6a
Rupam Singh <rupamkumar333@gmail.com>
2025-07-30 00:03:06 +0530

    testing2

--- middleware.ts
+++ middleware.ts
@@ -8,8 +8,6 @@ export async function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
   const adminToken = request.cookies.get('admin_token')?.value;
   const { pathname } = request.nextUrl;
-  console.log("test")
-
   console.log('>>>>>>>>>>>>');
   
 
